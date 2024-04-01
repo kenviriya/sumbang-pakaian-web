4a099ba6d700b6f0de6d74dc73e00ad8c75c2527
Ken Viriya <v.ken.viriya@adira.co.id>
2024-04-02 01:06:57 +0700

    SPW # add item card

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from './_components/navbar';
 
 const MarketingLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="h-full">
+    <div className="h-full bg-[#f8f7f4]">
       <Navbar />
       <main className="h-full pt-20">{children}</main>
     </div>
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,79 @@
 import Heading from './_components/heading';
+import ItemCard from './_components/itemCard';
 
 const HomePage = () => {
+  const items = [
+    {
+      imageUrl: '/carousel-1.jpg',
+      imageAlt: 'image-1',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro vero perspiciatis aut eius. Tempore ex iusto voluptatibus architecto earum velit quisquam ab ducimus repellendus, provident quae magnam illo. At architecto amet sapiente obcaecati nulla enim vel repellat sit iste nihil nesciunt necessitatibus, molestiae provident, ab rem, mollitia consequatur expedita excepturi incidunt. Nemo a voluptatum fugiat est qui ipsam doloribus eius expedita modi asperiores quasi at cumque maiores aperiam ut, corrupti laboriosam tempora accusantium nihil veniam, saepe harum. Sequi nulla, quae, rerum labore beatae aliquam iusto placeat explicabo minima reprehenderit minus. Harum quasi dolor, similique magni ut cum! Consequatur, nemo quia.',
+    },
+    {
+      imageUrl: '/carousel-2.jpg',
+      imageAlt: 'image-2',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque accusamus aliquid facilis illo quidem itaque blanditiis error maiores necessitatibus exercitationem dolores, ex magnam excepturi fuga minima repudiandae? Itaque minus necessitatibus porro consequuntur, corrupti tempora nemo possimus harum. Nostrum neque odit animi dolores ea repellat, minus similique, laudantium quasi, architecto ullam molestiae eaque eveniet omnis dolorum voluptates beatae temporibus quas debitis! Hic possimus vitae, iure a dicta, ut molestias ipsum non consequuntur at dolores alias qui. Est provident blanditiis perferendis nisi dicta debitis dolorum praesentium expedita perspiciatis rerum vel cum fuga doloremque non aliquam, ea, voluptatibus nobis. Aliquid id porro neque.',
+    },
+    {
+      imageUrl: '/carousel-3.jpg',
+      imageAlt: 'image-3',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, labore!',
+    },
+    {
+      imageUrl: '/carousel-1.jpg',
+      imageAlt: 'image-1',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, labore!',
+    },
+    {
+      imageUrl: '/carousel-1.jpg',
+      imageAlt: 'image-1',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, labore!',
+    },
+    {
+      imageUrl: '/carousel-2.jpg',
+      imageAlt: 'image-2',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, labore!',
+    },
+    {
+      imageUrl: '/carousel-3.jpg',
+      imageAlt: 'image-3',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, labore!',
+    },
+    {
+      imageUrl: '/carousel-1.jpg',
+      imageAlt: 'image-1',
+      title: 'Lorem ipsum dolor sit amet.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, labore!',
+    },
+  ];
   return (
-    <div className="bg-[#f8f7f4] h-full">
+    <div className="h-full w-full">
       <Heading />
+      <div className="px-[4%] bg-[#f8f7f4] flex flex-row flex-wrap justify-start gap-x-9 gap-y-6 pt-9">
+        {items.map((item, index) => (
+          <ItemCard
+            key={index}
+            imageUrl={item.imageUrl}
+            imageAlt={item.imageAlt}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </div>
     </div>
   );
 };
