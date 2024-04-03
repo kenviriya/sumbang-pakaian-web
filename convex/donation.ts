8e3acb73fc9f125bc64c1c3dfd14c44782376793
Ken Viriya <v.ken.viriya@adira.co.id>
2024-04-03 19:44:12 +0700

    SPW #create api get donation by id

--- convex/donation.ts
+++ convex/donation.ts
@@ -1,6 +1,7 @@
+import {v} from 'convex/values';
 import {query} from './_generated/server';
 
-const getAll = query({
+const getAllDonation = query({
   handler: async (ctx) => {
     const donation = await ctx.db.query('donation').collect();
     const donationRequests = await ctx.db.query('donation_request').collect();
@@ -34,4 +35,49 @@ const getAll = query({
   },
 });
 
-export {getAll};
+const getDonationById = query({
+  args: {
+    donationId: v.id('donation'),
+  },
+  handler: async (ctx, args) => {
+    const getDonation = await ctx.db.get(args.donationId);
+
+    if (!getDonation) {
+      throw new Error('Donation not found');
+    }
+    const donationRequests = await ctx.db.query('donation_request').collect();
+    const requestStatus = await ctx.db.query('ref_status').collect();
+
+    const tableJoin = donationRequests.map((donationRequest) => {
+      const relatedStatus = requestStatus.find(
+        (status) => status._id === donationRequest?.status
+      );
+      return {
+        donationRequest,
+        donationStatus: relatedStatus?.name,
+      };
+    });
+
+    const donationRequest = tableJoin.find(
+      (data) => data?.donationRequest?._id === getDonation.donationRequest
+    );
+
+    if (!donationRequest) {
+      throw new Error('Donation not found');
+    }
+
+    const donation = {
+      title: donationRequest?.donationRequest?.title,
+      description: donationRequest?.donationRequest?.description,
+      image: donationRequest?.donationRequest?.image,
+      startDate: donationRequest?.donationRequest?.startDate,
+      endDate: donationRequest?.donationRequest?.endDate,
+      requestStatus: donationRequest?.donationStatus,
+      donationStatus: getDonation.status === false ? 'Not Active' : 'Active',
+    };
+
+    return donation;
+  },
+});
+
+export {getAllDonation, getDonationById};
